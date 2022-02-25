42c28a015490814c99ff53c9c1160ba36f5d073a
magang_bayum <bayu.maulanaikhsan123@gmail.com>
2022-02-25 11:47:26 +0700

    git commit -m "Final"

--- logical_test/1_array_loop.php
+++ logical_test/1_array_loop.php
@@ -1,7 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-$aplikasi[1] = 'gitAkademik';
-$aplikasi[2] = 'gitFinansi';
-$aplikasi[3] = 'gitPerizinan';
-$aplikasi[4] = 'eCamopuz';
-$aplikasi[5] = 'eOviz';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $aplikasi[1] = 'gitAkademik';
+    $aplikasi[2] = 'gitFinansi';
+    $aplikasi[3] = 'gitPerizinan';
+    $aplikasi[4] = 'eCamopuz';
+    $aplikasi[5] = 'eOviz';
+
+    $i = 0;
+    while ($i < count($aplikasi)) {
+        echo $aplikasi[$i] . "<br>";
+        $i++;
+    }
+    ?>
+</body>
+
+</html>
